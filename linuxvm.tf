0a5c0f273da58dcb218c00876cb1b95c5a00e97b
mudassir <abc@xyz.com>
2025-07-30 13:26:56 +0530

    linuxvm file added

---
+++ linuxvm.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "example" {
+  name=var.rg_name
+  location = "westus"
+}
+# # resource "azurerm_linux_virtual_machine" "example" {
+# #     name = var.vm_name
+# #     resource_group_name = var.rg_name
+# #    location = azurerm_resource_group.example.location
+# #    size = "Standard_F2"
+# #    admin_admin_username = var.vm_user  
+# # }
