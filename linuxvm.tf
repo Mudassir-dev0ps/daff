3334e0cca91265b129a76712f97ef8cb5e1adb95
Mudassir-dev0ps <126445925+Mudassir-dev0ps@users.noreply.github.com>
2025-07-30 22:08:21 +1000

    Update linuxvm.tf

--- linuxvm.tf
+++ linuxvm.tf
@@ -2,6 +2,20 @@ resource "azurerm_resource_group" "example" {
   name=var.rg_name
   location = "westus"
 }
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
 # # resource "azurerm_linux_virtual_machine" "example" {
 # #     name = var.vm_name
 # #     resource_group_name = var.rg_name
